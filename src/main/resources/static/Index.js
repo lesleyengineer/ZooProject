33e801f0489b759c58aab0936abea348168279ab
lesleyferrie <lesleyengineer@outlook.com>
2021-08-09 16:04:13 +0100

    attempt to commit again by lf

--- src/main/resources/static/Index.js
+++ src/main/resources/static/Index.js
@@ -5,20 +5,20 @@ let jsonData;
 fetch(url)
 .then((response) => response.json())
 .then((data) => (jsonData = data))
-.then(() => callback(jsonData.data))
+.then(() => callback(jsonData))
 .catch((error) => console.log(error));
 }
 
-getData('localhost:9001/read', useData);
+getData('http://localhost:9001/read', useData);
+
 function useData(arrOfObjs) {
 const animalContainer = document.createElement('div');
-div.appendChild(animalContainer);
 console.log(arrOfObjs);
 arrOfObjs.forEach((animal) => {
 const pName = document.createElement('p');
 const pHabitat = document.createElement('p');
 const pFood = document.createElement('p');
-const pFood = document.createElement('p');
+const pAge = document.createElement('p');
 pName.innerText = animal.name;
 pHabitat.innerText = animal.habitat;
 pFood.innerFood = animal.food
@@ -27,5 +27,6 @@ animalContainer.appendChild(pName);
 animalContainer.appendChild(pHabitat);
 animalContainer.appendChild(pFood);
 animalContainer.appendChild(pAge);
-div.appendChild(personContainer);
+})
+div.appendChild(animalContainer);
 }
